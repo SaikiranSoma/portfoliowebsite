876596a62611d4c991d8ca1586a193c100b09908
SaikiranSoma <101874316+SaikiranSoma@users.noreply.github.com>
2023-03-07 00:20:06 +0530

    Final version
    Here is my portfolio website made using html,css,javascript.

---
+++ preloader.js
@@ -0,0 +1,5 @@
+//for loading animation
+setTimeout(function() {
+    document.querySelector('.no-scroll-loader').style.display = 'none';
+    document.querySelector('.main-content').style.display = 'block';
+  }, 3500);
